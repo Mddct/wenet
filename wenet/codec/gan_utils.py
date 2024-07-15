6b457cbdc5444f32cb5a651d68ba8730d2158374
Mddct <hamddct@gmail.com>
2024-07-15 13:29:34 +0800

    add conformer discriminators

--- wenet/codec/gan_utils.py
+++ wenet/codec/gan_utils.py
@@ -1,7 +1,10 @@
+from typing import Optional
 import torch
 from torch.nn.utils import weight_norm
+from wenet.transformer.encoder import ConformerEncoder
 
 from wenet.utils.class_utils import WENET_ACTIVATION_CLASSES
+from wenet.utils.mask import make_pad_mask
 
 
 class Discriminator(torch.nn.Module):
@@ -53,9 +56,79 @@ class Discriminator(torch.nn.Module):
         return x[:, 0], intermediate_outputs
 
 
+class ConformerDiscriminator(ConformerEncoder):
+
+    def __init__(self,
+                 input_size: int,
+                 output_size: int = 256,
+                 attention_heads: int = 4,
+                 linear_units: int = 2048,
+                 num_blocks: int = 3,
+                 dropout_rate: float = 0.1,
+                 positional_dropout_rate: float = 0.1,
+                 attention_dropout_rate: float = 0,
+                 input_layer: str = "linear",
+                 pos_enc_layer_type: str = "rel_pos",
+                 normalize_before: bool = True,
+                 static_chunk_size: int = 0,
+                 use_dynamic_chunk: bool = False,
+                 global_cmvn: torch.nn.Module = None,
+                 use_dynamic_left_chunk: bool = False,
+                 positionwise_conv_kernel_size: int = 1,
+                 macaron_style: bool = True,
+                 selfattention_layer_type: str = "rel_selfattn",
+                 activation_type: str = "swish",
+                 use_cnn_module: bool = True,
+                 cnn_module_kernel: int = 15,
+                 causal: bool = False,
+                 cnn_module_norm: str = "batch_norm",
+                 query_bias: bool = True,
+                 key_bias: bool = True,
+                 value_bias: bool = True,
+                 conv_bias: bool = True,
+                 gradient_checkpointing: bool = False,
+                 use_sdpa: bool = False,
+                 layer_norm_type: str = 'layer_norm',
+                 norm_eps: float = 0.00001,
+                 n_kv_head: Optional[int] = None,
+                 head_dim: Optional[int] = None,
+                 mlp_type: str = 'position_wise_feed_forward',
+                 mlp_bias: bool = True,
+                 n_expert: int = 8,
+                 n_expert_activated: int = 2):
+        super().__init__(
+            input_size, output_size, attention_heads, linear_units, num_blocks,
+            dropout_rate, positional_dropout_rate, attention_dropout_rate,
+            input_layer, pos_enc_layer_type, normalize_before,
+            static_chunk_size, use_dynamic_chunk, global_cmvn,
+            use_dynamic_left_chunk, positionwise_conv_kernel_size,
+            macaron_style, selfattention_layer_type, activation_type,
+            use_cnn_module, cnn_module_kernel, causal, cnn_module_norm,
+            query_bias, key_bias, value_bias, conv_bias,
+            gradient_checkpointing, use_sdpa, layer_norm_type, norm_eps,
+            n_kv_head, head_dim, mlp_type, mlp_bias, n_expert,
+            n_expert_activated)
+
+    def forward(self, x: torch.Tensor, x_lens: torch.Tensor):
+        masks = ~make_pad_mask(x_lens, x.size(1)).unsqueeze(1)  # (B, 1, T)
+        x, pos_emb, masks = self.embed(x, masks)
+
+        intermediate_outputs = []
+        for (_, layer) in enumerate(self.encoders):
+            x, _, _, _ = layer(x, masks, pos_emb)
+            intermediate_outputs.append(x)
+
+        return x, intermediate_outputs
+
+
 if __name__ == "__main__":
     model = Discriminator()
     print(sum(p.numel() for p in model.parameters()) / 1_000_000)
     x = torch.randn(1, 128, 1024)
     y, _ = model(x)
+    print(y.shape)
+
+    model = ConformerDiscriminator(input_size=1024, output_size=256)
+    x_lens = torch.tensor([128])
+    y, _ = model(x, x_lens)
     print(y.shape)
